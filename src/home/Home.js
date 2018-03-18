f23797918150be0d85ee8582f3bf151b3a4f8839
Jonjo McKay <jonjo@jonjomckay.com>
2018-03-18 13:26:13 +0000

    Moved dashboard tables into tabs

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HomeStates from "./HomeStates";
 import HomeStatesByFlow from "./HomeStatesByFlow";
+import { Tabs } from 'antd';
 
 export default class Home extends Component {
     render() {
@@ -8,8 +9,19 @@ export default class Home extends Component {
             <div>
                 <h1>Dashboard</h1>
 
-                <HomeStates />
-                <HomeStatesByFlow />
+                <Tabs defaultActiveKey="1">
+                    <Tabs.TabPane tab="All States" key="1">
+                        <HomeStates />
+                    </Tabs.TabPane>
+
+                    <Tabs.TabPane tab="By Flow" key="2">
+                        <HomeStatesByFlow />
+                    </Tabs.TabPane>
+
+                    <Tabs.TabPane tab="By # of Errors" key="3">
+
+                    </Tabs.TabPane>
+                </Tabs>
             </div>
         )
     }
